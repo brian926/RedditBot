1786a3f1d151d53d21c72f665a2768daef495fcb
Brian Antunes <brian.antunes@yahoo.com>
2022-07-01 07:34:48 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -18,3 +18,22 @@ def all():
 	print("Got result back...")
 
 	return render_template("index.html", Title=subreddit.capitalize(), result=result)
+
+@app.route("/", methods=('GET', 'POST'))
+def search():
+	if request.method == 'POST':
+		subreddit = request.form['subreddit']
+		print(subreddit)
+		if not subreddit:
+			flash('Enter a subreddit')
+		else:
+			return redirect(url_for('.test', id = subreddit))
+	return render_template("search.html")
+
+@app.route('/results/<id>')
+def test(id):
+	subreddit = id
+	result = bot.awaken_bot(subreddit)
+	print("Got result back...")
+
+	return render_template("index.html", Title=subreddit.capitalize(), result=result)
